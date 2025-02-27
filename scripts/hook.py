05c504e15db87d579b1beabf42e5a334e503f05a
mili-inch <hangoumesi@gmail.com>
2025-02-27 21:05:33 +0900

    simplify

--- scripts/hook.py
+++ scripts/hook.py
@@ -1,5 +1,4 @@
 from typing import List, Optional, Callable
-from enum import Enum
 import torch
 
 from transformers.models.clip.modeling_clip import (
@@ -12,45 +11,20 @@ from modules.shared import opts as shared_opts
 from modules.sd_emphasis import get_current_option
 
 
-class AttentionScope(Enum):
-    BOS = "BOS"
-    PROMPT = "Prompt"
-    EOS = "EOS"
-    PAD = "PAD"
-    ADDITIONAL_PAD = "Additional PAD"
-
-
-class AttentionWindowLimit(Enum):
-    UNLIMITED = "Unlimited"
-    POSITION_BASED_FROM_END = "PositionBasedFromEnd"
-    POSITION_BASED_FROM_START = "PositionBasedFromStart"
-
-
-class PositionalEncoding(Enum):
-    FROM_END = "FromEnd"
-    FROM_START = "FromStart"
-    UNUSED_FROM_END = "UnusedFromEnd"
-    UNUSED_FROM_START = "UnusedFromStart"
-    LAST_REPEAT = "LastRepeat"
-    NO_PE = "NoPE"
-    MEAN_REPEAT = "MeanRepeat"
-
-
 class IllustriousPadEnhancerHook:
     def __init__(self):
         self.original_encoder_forward_l: Optional[object] = None
         self.original_encoder_forward_g: Optional[object] = None
         self.original_embeddings_forward_l: Optional[object] = None
         self.original_embeddings_forward_g: Optional[object] = None
+        self.original_after_transformers: Optional[object] = None
         self.eos_position: Optional[int] = None
         self.previous_options: Optional[dict] = None
+        self.conds: List[dict] = []
 
     def _get_patched_clip_encoder_forward(
         self,
         original_encoder_forward: Callable,
-        pad_attention_scope: List[AttentionScope],
-        pad_attention_window_limit: AttentionWindowLimit,
-        pad_positional_encoding: PositionalEncoding,
     ):
         def forward(
             inputs_embeds,
@@ -60,72 +34,22 @@ class IllustriousPadEnhancerHook:
             output_hidden_states=None,
             return_dict=False,
         ):
+            # create new causal attention mask
             new_causal_attention_mask = _create_4d_causal_attention_mask(
                 (inputs_embeds.shape[0], inputs_embeds.shape[1]),
                 dtype=causal_attention_mask.dtype,
                 device=causal_attention_mask.device,
             )
 
-            # get mask
+            # get mask score
             mask = new_causal_attention_mask[
                 0, 0, 0, inputs_embeds.shape[1] - 1
             ].clone()
 
-            # apply mask to the attention mask for each scope
-            if AttentionScope.BOS.value not in pad_attention_scope:
-                new_causal_attention_mask[:, :, 77:, 0] = mask
-            if AttentionScope.PROMPT.value not in pad_attention_scope:
-                new_causal_attention_mask[:, :, 77:, 1 : self.eos_position] = mask
-            if AttentionScope.EOS.value not in pad_attention_scope:
-                new_causal_attention_mask[:, :, 77:, self.eos_position] = mask
-            if AttentionScope.PAD.value not in pad_attention_scope:
-                new_causal_attention_mask[:, :, 77:, self.eos_position + 1 : 77] = mask
-            if AttentionScope.ADDITIONAL_PAD.value not in pad_attention_scope:
-                new_causal_attention_mask[:, :, 77:, 77:] = mask
+            # additional pads must not attend to existing pads
+            new_causal_attention_mask[:, :, 77:, self.eos_position + 1 : 77] = mask
 
-            # apply attention window limit
-            additional_pad_length = inputs_embeds.shape[1] - 77
-            additional_pad_positions = []
-            if PositionalEncoding.FROM_START.value in pad_positional_encoding:
-                for i in range(additional_pad_length):
-                    additional_pad_positions.append(1 + i)
-            elif PositionalEncoding.FROM_END.value in pad_positional_encoding:
-                for i in range(additional_pad_length):
-                    additional_pad_positions.append(77 - additional_pad_length + i + 1)
-            elif PositionalEncoding.LAST_REPEAT.value in pad_positional_encoding:
-                for i in range(additional_pad_length):
-                    additional_pad_positions.append(77)
-
-            if (
-                pad_attention_window_limit
-                == AttentionWindowLimit.POSITION_BASED_FROM_START.value
-            ):
-                for i in range(additional_pad_length):
-                    not_masked_count = 0
-                    for j in range(inputs_embeds.shape[1]):
-                        if not_masked_count > additional_pad_positions[i]:
-                            new_causal_attention_mask[:, :, 77 + i, j] = mask
-                            continue
-                        if new_causal_attention_mask[:, :, 77 + i, j] >= 0:
-                            not_masked_count += 1
-            elif (
-                pad_attention_window_limit
-                == AttentionWindowLimit.POSITION_BASED_FROM_END.value
-            ):
-                for i in range(additional_pad_length):
-                    not_masked_count = 0
-                    for j in reversed(range(inputs_embeds.shape[1])):
-                        if not_masked_count > additional_pad_positions[i]:
-                            new_causal_attention_mask[:, :, 77 + i, j] = mask
-                            continue
-                        if new_causal_attention_mask[:, :, 77 + i, j] >= 0:
-                            not_masked_count += 1
-
-            # diagonal
-            for i in range(additional_pad_length):
-                new_causal_attention_mask[:, :, 77 + i, 77 + i] = 0
-
-            return original_encoder_forward(
+            encoder_outputs = original_encoder_forward(
                 inputs_embeds,
                 attention_mask,
                 new_causal_attention_mask,
@@ -133,6 +57,8 @@ class IllustriousPadEnhancerHook:
                 output_hidden_states,
                 return_dict,
             )
+            print(encoder_outputs.hidden_states)
+            return encoder_outputs
 
         return forward
 
@@ -143,53 +69,36 @@ class IllustriousPadEnhancerHook:
         id_end: int,
         id_pad: int,
         pad_length: int,
-        pad_positional_encoding: PositionalEncoding,
     ):
         pad_embed = embeddings.token_embedding.weight[id_pad]  # D
         position_embeddings = embeddings.position_embedding.weight
-        mean_position_embedding = position_embeddings.data.mean(dim=0)  # D
 
         def forward(
             input_ids=None,  # B, L
-            position_ids=None,  # None
-            inputs_embeds=None,  # None
+            position_ids=None,
+            inputs_embeds=None,
         ):
+            # save eos position for later use
             self.eos_position = (input_ids == id_end).nonzero()[0, 1].item()
+
+            # get original embeddings
             original_embeddings = original_embeddings_forward(
                 input_ids, position_ids, inputs_embeds
             )  # B, L, D
+
+            # create additional embeddings
             additional_embeddings = pad_embed.repeat(
                 original_embeddings.shape[0], pad_length, 1
+            ).to("cuda")
+            additional_position_embeddings = position_embeddings[-pad_length:].to(
+                "cuda"
             )
-            additional_position_embeddings = None
-            if pad_positional_encoding == PositionalEncoding.FROM_START.value:
-                additional_position_embeddings = position_embeddings[1 : pad_length + 1]
-            elif pad_positional_encoding == PositionalEncoding.FROM_END.value:
-                additional_position_embeddings = position_embeddings[-pad_length:]
-            elif pad_positional_encoding == PositionalEncoding.NO_PE.value:
-                additional_position_embeddings = torch.zeros(
-                    pad_length,
-                    position_embeddings.shape[1],
-                    device=original_embeddings.device,
-                )
-            elif pad_positional_encoding == PositionalEncoding.LAST_REPEAT.value:
-                additional_position_embeddings = position_embeddings[-1].repeat(
-                    pad_length, 1
-                )
-            elif pad_positional_encoding == PositionalEncoding.MEAN_REPEAT.value:
-                additional_position_embeddings = mean_position_embedding.repeat(
-                    pad_length, 1
-                )
-            else:
-                raise ValueError(
-                    f"Invalid positional encoding: {pad_positional_encoding}"
-                )
 
+            # extend embeddings
             extended_embeddings = torch.cat(
                 [
                     original_embeddings,
-                    additional_embeddings.to("cuda")
-                    + additional_position_embeddings.to("cuda"),
+                    additional_embeddings + additional_position_embeddings,
                 ],
                 dim=1,
             )
@@ -197,9 +106,10 @@ class IllustriousPadEnhancerHook:
 
         return forward
 
-    def _patch_emphasis(self):
-        emphasis = get_current_option(shared_opts.emphasis)
-
+    def _get_patched_emphasis_after_transformers(
+        self,
+        original_after_transformers: Callable,
+    ):
         def after_transformers(self):
             if self.z.shape[1] > 77:
                 z_temp = self.z[:, 77:, :]
@@ -209,8 +119,7 @@ class IllustriousPadEnhancerHook:
             else:
                 original_after_transformers(self)
 
-        original_after_transformers = emphasis.after_transformers
-        emphasis.after_transformers = after_transformers
+        return after_transformers
 
     def process(
         self,
@@ -218,9 +127,6 @@ class IllustriousPadEnhancerHook:
         pad_enable: bool,
         pad_enable_for: List[str],
         pad_length: int,
-        pad_attention_scope: List[AttentionScope],
-        pad_attention_window_limit: AttentionWindowLimit,
-        pad_positional_encoding: PositionalEncoding,
         pad_enable_normalization: bool,
     ) -> None:
         # clear prompt cache
@@ -228,9 +134,6 @@ class IllustriousPadEnhancerHook:
             "pad_enable": pad_enable,
             "pad_enable_for": pad_enable_for,
             "pad_length": pad_length,
-            "pad_attention_scope": pad_attention_scope,
-            "pad_attention_window_limit": pad_attention_window_limit,
-            "pad_positional_encoding": pad_positional_encoding,
             "pad_enable_normalization": pad_enable_normalization,
         }
 
@@ -260,6 +163,7 @@ class IllustriousPadEnhancerHook:
         text_model_g = (
             p.sd_model.forge_objects.clip.cond_stage_model.clip_g.transformer.text_model
         )
+        emphasis = get_current_option(shared_opts.emphasis)
 
         if not pad_enable:
             if self.original_encoder_forward_l is not None:
@@ -274,6 +178,9 @@ class IllustriousPadEnhancerHook:
             if self.original_embeddings_forward_g is not None:
                 text_model_g.embeddings.forward = self.original_embeddings_forward_g
                 self.original_embeddings_forward_g = None
+            if self.original_after_transformers is not None:
+                emphasis.after_transformers = self.original_after_transformers
+                self.original_after_transformers = None
             return
 
         if self.original_encoder_forward_l is None:
@@ -284,14 +191,11 @@ class IllustriousPadEnhancerHook:
             self.original_embeddings_forward_l = text_model_l.embeddings.forward
         if self.original_embeddings_forward_g is None:
             self.original_embeddings_forward_g = text_model_g.embeddings.forward
-
-        self._patch_emphasis()
+        if self.original_after_transformers is None:
+            self.original_after_transformers = emphasis.after_transformers
 
         text_model_l.encoder.forward = self._get_patched_clip_encoder_forward(
             self.original_encoder_forward_l,
-            pad_attention_scope,
-            pad_attention_window_limit,
-            pad_positional_encoding,
         )
         text_model_l.embeddings.forward = (
             self._get_patched_clip_text_embeddings_forward(
@@ -304,14 +208,10 @@ class IllustriousPadEnhancerHook:
                     "pad"
                 ],
                 pad_length,
-                pad_positional_encoding,
             )
         )
         text_model_g.encoder.forward = self._get_patched_clip_encoder_forward(
             self.original_encoder_forward_g,
-            pad_attention_scope,
-            pad_attention_window_limit,
-            pad_positional_encoding,
         )
         text_model_g.embeddings.forward = (
             self._get_patched_clip_text_embeddings_forward(
@@ -324,8 +224,10 @@ class IllustriousPadEnhancerHook:
                     "pad"
                 ],
                 pad_length,
-                pad_positional_encoding,
             )
+        )
+        emphasis.after_transformers = self._get_patched_emphasis_after_transformers(
+            self.original_after_transformers
         )
 
     def process_before_every_sampling(
@@ -333,11 +235,6 @@ class IllustriousPadEnhancerHook:
         p: StableDiffusionProcessing,
         pad_enable: bool,
         pad_enable_for: List[str],
-        pad_length: int,
-        pad_attention_scope: List[AttentionScope],
-        pad_attention_window_limit: AttentionWindowLimit,
-        pad_positional_encoding: PositionalEncoding,
-        pad_enable_normalization: bool,
         *args,
         **kwargs,
     ) -> None:
@@ -358,28 +255,6 @@ class IllustriousPadEnhancerHook:
                     uncond[i]["model_conds"]["c_crossattn"].cond = uncond[i][
                         "model_conds"
                     ]["c_crossattn"].cond[:, :77, :]
-
-            # normalize conditioning using the mean of the original conditioning
-            if "Positive Prompt" in pad_enable_for and pad_enable_normalization:
-                for i in range(len(cond)):
-                    original_mean = (
-                        cond[i]["model_conds"]["c_crossattn"].cond[:, :77, :].mean()
-                    )
-                    current_mean = cond[i]["model_conds"]["c_crossattn"].cond.mean()
-                    cond[i]["model_conds"]["c_crossattn"].cond = cond[i]["model_conds"][
-                        "c_crossattn"
-                    ].cond * (original_mean / current_mean)
-
-            if "Negative Prompt" in pad_enable_for and pad_enable_normalization:
-                for i in range(len(uncond)):
-                    original_mean = (
-                        uncond[i]["model_conds"]["c_crossattn"].cond[:, :77, :].mean()
-                    )
-                    current_mean = uncond[i]["model_conds"]["c_crossattn"].cond.mean()
-                    uncond[i]["model_conds"]["c_crossattn"].cond = uncond[i][
-                        "model_conds"
-                    ]["c_crossattn"].cond * (original_mean / current_mean)
-
             return model, x, timestep, uncond, cond, cond_scale, model_options, seed
 
         unet = p.sd_model.forge_objects.unet
--- scripts/main.py
+++ scripts/main.py
@@ -6,9 +6,6 @@ import modules.scripts as scripts
 
 from scripts.hook import (
     IllustriousPadEnhancerHook,
-    AttentionWindowLimit,
-    PositionalEncoding,
-    AttentionScope,
 )
 
 
@@ -18,13 +15,13 @@ class IllustriousPadEnhancer(scripts.Script):
         self.hook = IllustriousPadEnhancerHook()
 
     def title(self) -> str:
-        return "Illustrious Pad Enhancer"
+        return "Illustrious XL Pad Enhancer"
 
     def show(self, is_img2img: bool) -> object:
         return scripts.AlwaysVisible
 
     def ui(self, is_img2img: bool) -> List[gr.components.Component]:
-        with gr.Accordion("Illustrious Pad Enhancer", open=False):
+        with gr.Accordion("Illustrious XL Pad Enhancer", open=False):
             pad_enable = gr.Checkbox(value=False, label="Enable Pad Enhancer")
             pad_enable_for = gr.CheckboxGroup(
                 choices=["Positive Prompt", "Negative Prompt"],
@@ -34,41 +31,14 @@ class IllustriousPadEnhancer(scripts.Script):
             pad_length = gr.Slider(
                 minimum=1, maximum=75, step=1, label="Pad Length", value=15
             )
-            pad_attention_scope = gr.CheckboxGroup(
-                choices=[t.value for t in AttentionScope],
-                value=[
-                    AttentionScope.BOS.value,
-                    AttentionScope.PROMPT.value,
-                    AttentionScope.EOS.value,
-                    AttentionScope.ADDITIONAL_PAD.value,
-                ],
-                label="Attention Scope",
-            )
-            pad_attention_window_limit = gr.Radio(
-                choices=[t.value for t in AttentionWindowLimit],
-                value=AttentionWindowLimit.UNLIMITED.value,
-                label="Attention Window Limit",
-            )
-            pad_positional_encoding = gr.Radio(
-                choices=[t.value for t in PositionalEncoding],
-                value=PositionalEncoding.FROM_END.value,
-                label="Positional Encoding",
-            )
             pad_enable_normalization = gr.Checkbox(
                 value=False, label="Enable Normalization"
-            )
-            pad_to_additional_pad_attention = gr.Checkbox(
-                value=True, label="Pad to Additional Pad Attention"
             )
         return [
             pad_enable,
             pad_enable_for,
             pad_length,
-            pad_attention_scope,
-            pad_attention_window_limit,
-            pad_positional_encoding,
             pad_enable_normalization,
-            pad_to_additional_pad_attention,
         ]
 
     def process(
